66690d344096cac6dc09c0c8cc565afbfabdc6bd
Lando <jacquesalande@gmail.com>
2016-05-26 12:23:16 -0400

    Creation d'un formulaire

---
+++ formulaire.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8"/>
+<head>
+<title>formulaire</title>
+</head>
+<body
+
+<form action="" method="post">
+<p>
+    <input type="text" name="prenom" />
+    <input type="submit" value="Valider" />
+</p>
+</form>
